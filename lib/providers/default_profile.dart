52883063860b7c01d1ee6a09a673ebcdd70fe8d0
onecat <onecat@users.noreply.github.com>
2026-09-20 22:53:00 +0800

    refactor: load profiles in default profile initializer

--- lib/providers/default_profile.dart
+++ lib/providers/default_profile.dart
@@ -4,9 +4,9 @@ import 'package:fl_clash/models/models.dart';
 
 Future<({Config config, List<Profile> profiles})> ensureDefaultDirectProfile({
   required Config config,
-  required List<Profile> profiles,
   required bool isWindows,
 }) async {
+  final profiles = await database.profilesDao.query().get();
   if (!shouldCreateDefaultDirectProfile(
     isWindows: isWindows,
     hasProfiles: profiles.isNotEmpty,
