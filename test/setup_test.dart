743b39541938964f5a70a533c2a9d65615332d1b
onecat <onecat@users.noreply.github.com>
2026-09-20 20:50:20 +0800

    style: format setup tests

--- test/setup_test.dart
+++ test/setup_test.dart
@@ -22,11 +22,14 @@ void main() {
     });
 
     test('Flutter build environment includes UI visibility flags', () {
-      expect(setup.createBuildEnvironment('dev'), {
-        'APP_ENV': 'dev',
-        'HIDE_ABOUT': false,
-        'HIDE_DISCLAIMER': false,
-      });
+      expect(
+        setup.createBuildEnvironment('dev'),
+        {
+          'APP_ENV': 'dev',
+          'HIDE_ABOUT': false,
+          'HIDE_DISCLAIMER': false,
+        },
+      );
       expect(
         setup.createBuildEnvironment(
           'stable',
